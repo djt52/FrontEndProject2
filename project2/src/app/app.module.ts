2dc27f4d7cb2a0a8784b02bbab82130d0951d8ec
Daniel <djt52@case.edu>
2018-01-13 23:26:32 -0500

    First Commit

---
+++ project2/src/app/app.module.ts
@@ -0,0 +1,37 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { MainComponent } from './main/main.component';
+import { AccountComponent } from './account/account.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { LoginComponent } from './login/login.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    LeaderboardComponent,
+    MainComponent,
+    AccountComponent,
+    QuizComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: MainComponent},
+      {path: 'login', component: LoginComponent}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ project2/src/app/leaderboard/leaderboard.component.ts
@@ -0,0 +1,31 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leaderboard',
+  templateUrl: './leaderboard.component.html',
+  styleUrls: ['./leaderboard.component.css'],
+})
+export class LeaderboardComponent implements OnInit {
+  leader = [new Leaders('John',1000), new Leaders('Jim',900)];
+
+  constructor() { }
+
+  ngOnInit() {
+  //leaders = [{"name":"John", "score":1000}, {"name":"Jim", "score":900}, {"name":"Luke", "score":800}, {"name":"Ron", "score":700},];
+  //leader=[new Leaders('John',1000), new Leaders('Bob',900)];
+  }
+   // public leader: Leaders[];
+}
+
+export class Leaders {
+  //public showLeaders()
+  public name: string;
+  public score: number;
+  
+  constructor(s: string, n: number) {
+      this.name=s;
+      this.score=n;
+  }
+  
+
+}
