39f3af92a6db567c6aca02df7cf936b9ada8dd40
Daniel <djt52@case.edu>
2018-01-18 11:50:19 -0500

    Quiz Observable set up

--- project2/src/app/question/question.component.ts
+++ project2/src/app/question/question.component.ts
@@ -1,4 +1,7 @@
 ﻿import { Component, OnInit } from '@angular/core';
+import { QuestionsService } from '../questions.service';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
 @Component({
   selector: 'app-question',
@@ -7,14 +10,49 @@
 })
 export class QuestionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private questionsService: QuestionsService) { }
 
   ngOnInit() {
+  /*
     let jsondata = `[{"question":"Arasilly is stupid.Loream Ipsum Fataneums. AWDADWADADAWDAWDWDAWD", "option1":1,"option2":2,"option3":3,"option4":4,"answer":4},{"question":"3+2", "option1":1,"option2":2,"option3":5,"option4":4,"answer":3}]`;
     this.questions = JSON.parse(jsondata);
+    this.questionsService.getQuestions();
+    */
+    //this.questionGet(this.parse);
+    this.index = 0;
+    this.questionsService.getQuestions().subscribe(
+        res => {
+          console.log(res);
+          this.questions = res[0];
+          console.log(this.questions);   
+          this.questionAnswered(0);
+         }
+    );
+    console.log(this.question);
+  }
+  /*
+  questionGet(cfunction) {
+    let xml = new XMLHttpRequest();
+    xml.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        cfunction(this);
+       }
+    };
+    xml.open('GET',`http://54.198.14.22:8090/api/questionslist?quiz=1`,true);
+    xml.send();
+  }
+  
+  parse(xhttp) {
+    let jsondata = xhttp.responseText;
+    this.questions=JSON.parse(jsondata);
+    this.questionAnswered(0);
+  }
+
+  
     this.index = 0;
     this.questionAnswered(0);
   }
+  */
   public question: string;
   public option1: string;
   public option2: string;
@@ -27,12 +65,13 @@ export class QuestionComponent implements OnInit {
   //NgStyle 
   questionAnswered(answer: number) {
       //if(this.index == 0) {
-      console.log(this.index)
-      this.question = this.questions[this.index].question;
-      this.option1 = this.questions[this.index].option1;
-      this.option2 = this.questions[this.index].option2;
-      this.option3 = this.questions[this.index].option3;
-      this.option4 = this.questions[this.index].option4;
+      console.log(this.index);
+      console.log(this.questions)
+      this.question = this.questions.question;
+      this.option1 = this.questions.choice1;
+      this.option2 = this.questions.choice2;
+      this.option3 = this.questions.choice3;
+      this.option4 = this.questions.choice4;
       this.index++;
       
   }
--- project2/src/app/questions.service.ts
+++ project2/src/app/questions.service.ts
@@ -1,16 +1,28 @@
 ﻿import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class QuestionsService {
 
-  constructor() { }
-
-  getQuestions(): Observable<Question[]> {
-      let jsondata = `[{"question":"2+2", "option1":1,"option2":2,"option3":3,"option4":4,"answer":4},{"question":"3+2", "option1":1,"option2":2,"option3":5,"option4":4,"answer":3}]`;
-      return JSON.parse(jsondata);
+  constructor(private http: HttpClient) { }
+  public questions
+  getQuestions(): Observable<any> {
+      //let jsondata = `[{"question":"2+2", "option1":1,"option2":2,"option3":3,"option4":4,"answer":4},{"question":"3+2", "option1":1,"option2":2,"option3":5,"option4":4,"answer":3}]`;
+      //return JSON.parse(jsondata);
       //return this.http.get<Question[]>(/*url*/);
+     /*
+      let xml = new XMLHttpRequest();
+      xml.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          console.log(this.responseText)
+       }
+    };
+      xml.open('GET',`http://54.198.14.22:8090/api/questionslist?quiz=1`,true);
+      xml.send();
+      */
+      return this.http.get(`http://54.198.14.22:8090/api/questionslist?quiz=1`);
   }
 }
 
