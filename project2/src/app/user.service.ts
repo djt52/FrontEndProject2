870e22d7457125c750e79bc6c14f7f57895feedb
Daniel <djt52@case.edu>
2018-01-19 10:31:16 -0500

    All function done, maybe a few style changes left

--- project2/src/app/user.service.ts
+++ project2/src/app/user.service.ts
@@ -16,14 +16,16 @@ export class UserService {
   login(email: string, password: string): Observable<any> {
     let observe = this.http.get(`http://54.198.14.22:8090/api/login?email=${email}&password=${password}`);
     observe.subscribe(
-        res => {this.loggedUser = res}
+        res => {this.loggedUser = res;
+        console.log(res);
+        }
     );
     return observe;
   }
   
 
   register(email: string, password: string, first:string, last: string) {
-    return this.http.get(`http://54.198.14.22:8090/api/login?email=${email}&password=${password}&fname=${first}&lname=${last}`);
+    return this.http.get(`http://54.198.14.22:8090/api/register?email=${email}&password=${password}&fname=${first}&lname=${last}`);
   }
 
   correct() {
@@ -48,8 +50,17 @@ export class UserService {
 
   updateUser() {
       if(this.loggedUser) {
-         console.log("Update");
-         this.http.get(`http://54.198.14.22:8090/api/updateUser?userid={this.loggedUser.id}&correctAnswers={right}&wrongAnswers={wrong}`);
+        let mail = this.loggedUser.email;
+        let ui = this.loggedUser.userId;
+        let ri = this.right + this.loggedUser.correctAnswers;
+        let wr = this.wrong + this.loggedUser.wrongAnswers;
+        console.log(this.loggedUser.email)
+         console.log(this.loggedUser.userId);
+         console.log(this.right);
+         console.log(this.wrong);
+         this.http.get(`http://54.198.14.22:8090/api/updateUser?userid=${ui}&correctAnswers=${ri}&wrongAnswers=${wr}`).subscribe(
+         res => {console.log("called update");}
+         );
       }
   }
 }
